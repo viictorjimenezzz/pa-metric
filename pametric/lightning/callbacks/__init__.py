6d93e9ba15e082a8d954831858732eddb021f398
Víctor Jiménez Rodríguez <victor.jimenez.rodriguez@estudiantat.upc.edu>
2025-03-25 12:19:05 +0100

    Update __init__.py

--- pametric/lightning/callbacks/__init__.py
+++ pametric/lightning/callbacks/__init__.py
@@ -1,5 +1,5 @@
 from pametric.lightning.callbacks.model_checkpoint import PA_ModelCheckpoint
 from pametric.lightning.callbacks.metric import PA_Callback
-from pametric.lightning.callbacks.batch_size_finder import MultienvBatchSizeFinder
+# from pametric.lightning.callbacks.batch_size_finder import MultienvBatchSizeFinder # error in newer versions of pytorch-lightning #TODO: fix
 from pametric.lightning.callbacks.measure_dataset import *
 from pametric.lightning.callbacks.measure_output import *
